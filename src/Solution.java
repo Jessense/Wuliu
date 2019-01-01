bb6c9b8ea06b35dbf5f5e8f59e6fd1d4c2185abd
CHy-KK <45690293+CHy-KK@users.noreply.github.com>
2019-01-01 20:32:02 +0800

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,38 @@
-
-public class Order {
-	private int order_id;
+public class Solution {
 	private int seller_id;
 	private int delivery_id;
 	private int transportation_type;
-	private int status; //订单状态，0: made, 1: transferring, 2: finished
+	private int speed; //预计运输速度
 	private int type; //货物种类
 	private int weight;
 	private int price;
 	private char start;
 	private char destination;
+	private long generatetime;
 	
-	public Order(int seller_id, int delivery_id, int transportation_type, int status, int type, int weight, int price, char start, char destination) {
+	public Solution(int seller_id, int delivery_id, int transportation_type, int speed, int type, int weight, int price, char start, char destination, long generatetime) {
 		// TODO Auto-generated constructor stub
-		this.order_id = /*something*/;
 		this.seller_id = seller_id;
 		this.delivery_id = delivery_id;
 		this.transportation_type = transportation_type;
-		this.status = status;
+		this.speed = speed;
 		this.type = type;
 		this.weight = weight;
 		this.price = price;
 		this.start = start;
-		this.destination =destination;
+		this.destination = destination;
+		this.generatetime = generatetime;
 	}
-	
 	public int getDelivery_id() {
 		return delivery_id;
-	}
-	public int getOrder_id() {
-		return order_id;
-	}
-	public int getPrice() {
-		return price;
 	}
 	public int getSeller_id() {
 		return seller_id;
 	}
-	public int getStatus() {
-		return status;
+	public int getSpeed() {
+		return speed;
 	}
-	public int getTransportation_id() {
+	public int getTransportation_type() {
 		return transportation_type;
 	}
 	public int getType() {
@@ -49,28 +41,28 @@ public class Order {
 	public int getWeight() {
 		return weight;
 	}
+	public int getPrice() {
+		return price;
+	}
 	public char getStart() {
 		return start;
 	}
 	public char getDestination() {
 		return destination;
 	}
+	public long getGeneratetime() {
+		return generatetime;
+	}
 	public void setDelivery_id(int delivery_id) {
 		this.delivery_id = delivery_id;
-	}
-	public void setOrder_id(int order_id) {
-		this.order_id = order_id;
-	}
-	public void setPrice(int price) {
-		this.price = price;
 	}
 	public void setSeller_id(int seller_id) {
 		this.seller_id = seller_id;
 	}
-	public void setStatus(int status) {
-		this.status = status;
+	public void setSpeed(int speed) {
+		this.speed = speed;
 	}
-	public void setTransportation_id(int transportation_type) {
+	public void setTransportation_type(int transportation_type) {
 		this.transportation_type = transportation_type;
 	}
 	public void setType(int type) {
@@ -85,4 +77,8 @@ public class Order {
 	public void setDestination(char destination) {
 		this.destination = destination;
 	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
 }

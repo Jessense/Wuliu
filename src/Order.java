a09fcee1f6050ac1a69443e1dab9629709a70501
CHy-KK <45690293+CHy-KK@users.noreply.github.com>
2019-01-04 21:43:41 +0800

    Update Order.java
    增加delivery_id属性及相关方法，应在doOrder方法中在某交通工具决定到达某个节点后，把装载的订单使用setDelivery_id方法置值

--- src/Order.java
+++ src/Order.java
@@ -5,6 +5,7 @@ public class Order extends Solution{
 	private int transportation_id;
 	private int status; //订单状态，0: made, 1: transferring, 2: finished
 	private int generatetime;
+	private int delivery_id
 	
 	/*计时起点：0；计时起点之后、大批量优惠区间内：1；大批量优惠区间外：-1。
 	 * 卖家确认第一个订单时，设为0；之后确认订单时，若前一个订单flag=-1，则该订单flag设为0，否则flag设为1。
@@ -15,9 +16,9 @@ public class Order extends Solution{
 	private int flag; 
 	
 	
-	public Order(int seller_id, int delivery_id, int transportation_type, int speed,int transportation_id, int status, int type, int weight, int price, char start, char destination) {
+	public Order(int seller_id, int transportation_type, int speed,int transportation_id, int status, int type, int weight, int price, char start, char destination) {
 		// TODO Auto-generated constructor stub
-		super(seller_id, delivery_id, transportation_type, speed, type, weight, price, start, destination);
+		super(seller_id, transportation_type, speed, type, weight, price, start, destination);
 		
 	}
 	
@@ -36,6 +37,9 @@ public class Order extends Solution{
 	public int getFlag() {
 		return flag;
 	}
+	public int getDelivery_id() {
+		return delivery_id;
+	}
 	public void setOrder_id(int order_id) {
 		this.order_id = order_id;
 	}
@@ -50,6 +54,9 @@ public class Order extends Solution{
 	}
 	public void setFlag(int flag) {
 		this.flag = flag;
+	}
+	public void setDelivery_id(int delivery_id) {
+		this.delivery_id = delivery_id;
 	}
 
 }
